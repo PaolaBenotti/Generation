a986bf71191c9acbf6cbdef82f7a057af2945a16
unknown <p.benotti@hotmail.com>
2021-07-27 10:23:13 -0300

    Rotas

---
+++ Modulo3/Angular/MeuBlog/src/app/rotas/rotas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rotas',
+  templateUrl: './rotas.component.html',
+  styleUrls: ['./rotas.component.css']
+})
+export class RotasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
